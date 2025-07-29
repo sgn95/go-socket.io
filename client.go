2bd97ce13689fd953991da753525e471eb38ce7f
宋光暖 <30686269+sgn95@users.noreply.github.com>
2025-07-29 10:47:54 +0800

    modify

--- client.go
+++ client.go
@@ -41,7 +41,7 @@ func NewClient(addr string, opts *engineio.Options) (*Client, error) {
 	namespace := fmtNS(u.Path)
 
 	// Not allowing other than default
-	u.Path = path.Join("/socket.io", namespace)
+	u.Path = path.Join(namespace, "/socket.io")
 	u.Path = u.EscapedPath()
 	if strings.HasSuffix(u.Path, "socket.io") {
 		u.Path += "/"
